6497b7e31e113b073df86c2a0fc3555b74a2d587
StoneLabs <levyehrstein@googlemail.com>
2021-03-06 18:23:50 +0100

    Add chat boardcast information on death

--- src/main/java/net/stone_labs/delayedrespawn/mixin/PlayerDeathMixin.java
+++ src/main/java/net/stone_labs/delayedrespawn/mixin/PlayerDeathMixin.java
@@ -1,6 +1,7 @@
 package net.stone_labs.delayedrespawn.mixin;
 
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.network.MessageType;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
@@ -10,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static net.stone_labs.delayedrespawn.DeathTimeManger.readDeathTimeoutConfig;
 import static net.stone_labs.delayedrespawn.DeathTimeManger.registerDeath;
 
 @Mixin(ServerPlayerEntity.class)
@@ -24,5 +26,11 @@ public class PlayerDeathMixin
 
         registerDeath(player);
         player.networkHandler.disconnect(new LiteralText("You died :(\nYou can reconnect once your cooldown has expired."));
+
+        long timeout = readDeathTimeoutConfig();
+        server.getPlayerManager().broadcastChatMessage(new LiteralText(
+                String.format("%s died and has to take %sh %sm %ss timeout.",
+                        player.getEntityName(), timeout / 60 / 60, timeout / 60 % 60, timeout % 60)
+        ).formatted(Formatting.RED), MessageType.CHAT, player.getUuid());
     }
 }
