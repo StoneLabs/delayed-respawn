bc9165ad4288eaa48e546ee2ba70ca7985226794
StoneLabs <git@sobadev.com>
2022-11-23 20:34:11 +0100

    Update to MC 1.19.2; Release 1.4.0

--- src/main/java/net/stone_labs/delayedrespawn/mixin/PlayerDeathMixin.java
+++ src/main/java/net/stone_labs/delayedrespawn/mixin/PlayerDeathMixin.java
@@ -1,10 +1,10 @@
 package net.stone_labs.delayedrespawn.mixin;
 
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.network.MessageType;
+import net.minecraft.network.message.MessageType;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.stone_labs.delayedrespawn.DelayedRespawn;
 import net.stone_labs.delayedrespawn.deathtime.DeathTimeFile;
@@ -26,12 +26,12 @@ public class PlayerDeathMixin
         assert server != null;
 
         DeathTimeFile.getInstance().registerDeath(player);
-        player.networkHandler.disconnect(new LiteralText("You died :(\nYou can reconnect once your cooldown has expired."));
+        player.networkHandler.disconnect(Text.literal("You died :(\nYou can reconnect once your cooldown has expired."));
 
         long timeout = DelayedRespawn.getDeathTimeoutLength(server);
-        server.getPlayerManager().broadcast(new LiteralText(
+        server.getPlayerManager().broadcast(Text.literal(
                 String.format("%s died and has to take %sh %sm %ss timeout",
                         player.getEntityName(), timeout / 60 / 60, timeout / 60 % 60, timeout % 60)
-        ).formatted(Formatting.RED), MessageType.CHAT, player.getUuid());
+        ).formatted(Formatting.RED), false);
     }
 }
